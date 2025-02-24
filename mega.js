a9e53acdf97f1f8b6e91c922ad71298d075cc32e
vimukthioshada <77786139+vimukthioshada@users.noreply.github.com>
2025-02-24 11:16:34 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,8 +1,8 @@
 const mega = require("megajs");
 
 const auth = {
-    email: 'your mega email',   //use your real vaild mega account email
-    password: 'mega password',  ////use your real vaild mega account password
+    email: 'vimukthioshada307@gmail.com',   //use your real vaild mega account email
+    password: 'Oshada2005@',  ////use your real vaild mega account password
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
